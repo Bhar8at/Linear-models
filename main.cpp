80335dff4756d7b97f01ce9b7feeffb87532e658
Bharat Deleep Nair <bhtnair@gmail.com>
2024-11-05 01:49:55 +0300

    Updated submodule

--- main.cpp
+++ main.cpp
@@ -16,13 +16,7 @@ float magnitude (intRow A){
         return sum;
     }
 
-float Rowdot (intRow Row1, intRow Row2){
-    float sum = 0;
-    for (int i = 0; i < Row1.size(); i++){
-        sum += (Row1[i] * Row2[i]);
-    }
-    return sum;
-}
+
 
 int main(){
     // // Opening the File
@@ -87,71 +81,6 @@ int main(){
     Training the MODEL
     */
 
-
-    // QR Decomposition
-    IntMatrix A = FillMatrix();
-    IntMatrix B = transpose(A);
-    intRow u,e;
-    IntMatrix E;
-
-    
-
-    for (int i = 0 ; i < B.size(); i++){
-        // Calculating the U
-        // Creating a Default Sum row
-        printf("\n Finding E%d:\n", i+1);
-        intRow Sum;
-
-        for (int j = 0; j < B[i].size(); j++){
-            Sum.push_back(0);
-        }
-
-        //Building the subtracting part
-        for (int j = 0; j < E.size(); j++){
-            float temp = Rowdot(B[i], E[j]);
-            printf("The dot product is %f\n",temp);
-            intRow tempa = Rowmultiply(E[j],temp);
-            Sum =  RowadddRow(Sum,tempa);
-        }
-
-        printf("\n subtracting ");
-        DisplayRow(B[i]);
-        DisplayRow(Sum);
-        printf("\n");
-
-        u = RowsubRow(B[i],Sum);
-        float modu = magnitude(u);
-        e = Rowdiv(u,modu);
-        E.push_back(e);
-    
-    }
-        printf("Done for Realsies");
-
-        // IntMatrix Q = transpose(E);
-
-        // Constructing R
-        IntMatrix R;
-        intRow R_row;
-        for (int i = 0 ; i < E.size(); i++){
-            for (int j = 0; j < E.size(); i++){
-            if (i < j){
-                R_row.push_back(0);
-            }
-            else{
-                printf("%d %d",j,i);
-                int tempr = Rowdot(B[j],E[i]);
-                R_row.push_back(tempr);
-                }
-            }
-            R.push_back(R_row);
-            R_row.clear();
-        }
-
-    
-
-    DisplayMatrix(E);
-    printf("\n\n");
-    DisplayMatrix(R);
 
 
 
