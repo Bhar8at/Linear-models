94bca286c675d200a7e01f98139ee1ba34f246fb
Bharat Deleep Nair <bhtnair@gmail.com>
2024-11-10 22:30:45 +0530

    Added Logistic Regression using Gradient Ascent

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "MultipleLinearRegression/MultipleLinearRegression.cpp"
 #include "LogisticRegression/LogisticRegression.cpp"
-#include "LogisticRegression/test.cpp"
+
 int main(){
     
     // MultipleLinearRegression();
